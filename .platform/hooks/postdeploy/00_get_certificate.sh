f0d1c630bc31df7bbee04e1fede74a6d6a29eb2a
caydnbaldwin <caydnbaldwin@gmail.com>
2025-12-03 15:38:02 -0700

    Fixed certbot misconfiguration.

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d dev.ellarises.net --nginx --agree-tos --email admin@bromunity.app
+sudo certbot -n -d ellarises.net --nginx --agree-tos --email admin@bromunity.app
