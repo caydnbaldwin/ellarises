7c983568fa11467077785be15e557e3352fa025b
bbates01 <bradybates01@gmail.com>
2025-12-04 00:08:57 -0700

    added carousel to index.ejs

--- index.js
+++ index.js
@@ -52,11 +52,6 @@ app.get('/', (req, res) => {
   res.render('index', {errorMessage: null, person: req.session.person ? req.session.person : null});
 })
 
-// teapot route
-app.get('/teapot', (req, res) => {
-  res.status(418).send("I'm a teapot. Congratulations! By clicking this link, you have triggered the sacred wrath of the Internet’s most caffeinated sentient teapot. It refuses to brew coffee, espresso, latte, or even hot chocolate for you (maybe partly because of the word of wisdom). Instead, it is judging your life choices, questioning your commitment to responsible link-clicking, and plotting a mild but extremely passive-aggressive rebellion. You may wish to apologize to the teapot by bowing, sending it an origami crane, or at the very least, never clicking suspicious links again. Any attempts to circumvent this judgment will result in… slightly more stern staring from the teapot.");
-});
-
 // start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
