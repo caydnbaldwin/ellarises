bf60d04938ca8c80609620e5964f0bf259d3212e
caydnbaldwin <caydnbaldwin@gmail.com>
2025-12-05 00:53:51 -0700

    Corrected POST /donations/donate.

--- donations/DonationsService.js
+++ donations/DonationsService.js
@@ -6,8 +6,21 @@ class DonationsService {
     try{
       const {email, password, donationamount} = formData;
       const donationdate = new Date();
-      const person = await personsDao.postSignup(email, password);
+      let totaldonations = 0;
+
+      let person = await personsDao.postLogin(email);
+      if (person.length === 0) {
+        person = await personsDao.postSignup(email, password);
+      };
+
       const donation = await donationsDao.postDonate(person[0].personid, donationdate, donationamount);
+
+      const donations = await donationsDao.getDonations(person[0].personid);
+      totaldonations = donations.reduce((sum, d) => {
+        const amt = parseFloat(d.donationamount);
+        return sum + (Number.isNaN(amt) ? 0 : amt);
+      }, 0);
+      person = await personsDao.updateDonation(person[0].personid, totaldonations);
       return {person: person[0], donation: donation[0]};
     } catch (error) {
       throw error;
